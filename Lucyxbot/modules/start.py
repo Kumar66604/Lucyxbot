888f9e43592ad127cb1e4f6effca26c0ebb79577
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-13 18:23:49 +0530

    Update start.py

--- Lucyxbot/modules/start.py
+++ Lucyxbot/modules/start.py
@@ -7,17 +7,17 @@ from config import PIC, BOT_USERNAME
 @app.on_message(filters.command("start"))
 async def start(_, msg):
     buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'https://t.me/{BOT_USERNAME}?startgroup=true')
+            InlineKeyboardButton('🥀 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘs 🥀', url=f'https://t.me/{BOT_USERNAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 support', url='https://t.me/vkmovies02'),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/vkmovies2')
+            InlineKeyboardButton('🥀 Sᴜᴘᴘᴏʀᴛ 🥀', url='https://t.me/vkmovies02'),
+            InlineKeyboardButton('🥀 Uᴘᴅᴀᴛᴇs 🥀', url='https://t.me/vkmovies2')
     ],[
-            InlineKeyboardButton('⚡ COMMANDS ⚡', url=f'https://t.me/{BOT_USERNAME}?start=true')
+            InlineKeyboardButton('✨ COMMANDS ✨', url=f'https://t.me/{BOT_USERNAME}?start=true')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await msg.reply_photo(
         photo=random.choice(PIC),
-        caption="""𝙷𝙴𝙻𝙾 {},\n👋 Welcome to <a href='https://t.me/lucy666xbot'>➳❥ɪᴹ᭄𝑳𝒖𝒄𝒚 𝒙 𝒃𝒐𝒕 🫧</a>,\n\n<b>I am here to assist you with various tasks.Feel free to explore the available commands and features.</b>\n\n""".format(msg.from_user.mention),
+        caption="""𝙷𝙴𝚈 𝚃𝙷𝙴𝚁𝙴! 🥀 {},\n👋 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝒕𝒐 <a href='https://t.me/lucy666xbot'>➳❥ɪᴹ᭄𝑳𝒖𝒄𝒚 𝒙 𝒃𝒐𝒕 🫧</a>,\n\n<b>𝙸 𝚊𝚖 𝚑𝚎𝚛𝚎 𝚝𝚘 𝚊𝚜𝚜𝚒𝚜𝚝 𝚢𝚘𝚞 𝚠𝚒𝚝𝚑 𝚟𝚊𝚛𝚒𝚘𝚞𝚜 𝚝𝚊𝚜𝚔𝚜. 𝙵𝚎𝚎𝚕 𝚏𝚛𝚎𝚎 𝚝𝚘 𝚎𝚡𝚙𝚕𝚘𝚛𝚎 𝚝𝚑𝚎 𝚊𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚊𝚗𝚍 𝚏𝚎𝚊𝚝𝚞𝚛𝚎𝚜.</b>\n\n""".format(msg.from_user.mention),
         reply_markup=reply_markup
     )
 
