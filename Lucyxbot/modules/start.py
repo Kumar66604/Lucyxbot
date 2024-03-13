d0aba2ff28de70f86b5e03d5ffd7e07bef53bb1c
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-13 16:03:18 +0530

    Update start.py

--- Lucyxbot/modules/start.py
+++ Lucyxbot/modules/start.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Lucyxbot import app
-from config import PIC
+from config import PIC, BOT_USERNAME
 
 @app.on_message(filters.command("start"))
-if len(message.command) != 2
+async def start(_, msg):
     buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'https://t.me/{BOT_USERNAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 support', url='https://t.me/vkmovies02'),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/vkmovies2')
@@ -15,6 +15,7 @@ if len(message.command) != 2
         await message.reply_photo(
             photo=random.choice(PIC),
             caption="""𝙷𝙴𝙻𝙾 {},\n𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍""",
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-)
+            reply_markup=reply_markup
+        )
+
+
