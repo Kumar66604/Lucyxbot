95f22c35329b24236b3110572f2280bc001eac83
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-13 12:34:04 +0530

    Update __init__.py

--- Lucyxbot/__init__.py
+++ Lucyxbot/__init__.py
@@ -1,11 +1,1 @@
-from pyrogram import Client, filters
-from config import *
 
-app = Client(
-    "Lucyxbot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
-
-print("🐾 I'm Alive Now 😀 🐾")
