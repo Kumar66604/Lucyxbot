a45f4e281ce7e1317214c4b19af676ba2dfa177f
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-13 16:27:26 +0530

    Update start.py

--- Lucyxbot/modules/start.py
+++ Lucyxbot/modules/start.py
@@ -4,6 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Lucyxbot import app
 from config import PIC, BOT_USERNAME
 
+START_TXT=f"""𝙷𝙴𝙻𝙾 {},\n𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍""",
 
 @app.on_message(filters.command("start"))
 async def start(_, msg):
@@ -16,7 +17,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     await msg.reply_photo(
         photo=random.choice(PIC),
-        caption="""𝙷𝙴𝙻𝙾 {},\n𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍""",
+        caption=START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
         reply_markup=reply_markup
     )
 
