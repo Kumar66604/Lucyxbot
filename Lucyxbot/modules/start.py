95938896ab317d07b77d35837fde15a0393547e8
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-13 16:13:23 +0530

    Update start.py

--- Lucyxbot/modules/start.py
+++ Lucyxbot/modules/start.py
@@ -12,7 +12,7 @@ async def start(_, msg):
             InlineKeyboardButton('🤖 Updates', url='https://t.me/vkmovies2')
 ]]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply_photo(
+    await msg.reply_photo(
         photo=random.choice(PIC),
         caption="""𝙷𝙴𝙻𝙾 {},\n𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍""",
         reply_markup=reply_markup
