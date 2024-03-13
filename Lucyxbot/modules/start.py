ae9435c68304628f490b6cdecae6a269850de895
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-13 15:35:28 +0530

    Update start.py

--- Lucyxbot/modules/start.py
+++ Lucyxbot/modules/start.py
@@ -3,5 +3,17 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Lucyxbot import app
 
 @app.on_message(filters.command("st"))
-async def start(_, message):
-    await message.reply_text("Hello! I am your bot. How can I help you?")
+buttons = [[
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],[
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=random.choice(PIC),
+            caption="""𝙷𝙴𝙻𝙾 {},
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍""", temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+)
