6b97688987f2f55e1158386ed1e9d1f564773ea4
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-14 01:26:47 +0530

    Update chatgpt.py

--- Lucyxbot/modules/chatgpt.py
+++ Lucyxbot/modules/chatgpt.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}\n        ❤️ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛ @lucy666xbot",
+                        f" {x}\n 💗 ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛ @lucy666xbot",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
