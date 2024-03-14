eb393a91d802f4a3ec4eb90e9ee7ca7ec60f5169
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-15 00:47:04 +0530

    Update start.py

--- Lucyxbot/modules/start.py
+++ Lucyxbot/modules/start.py
@@ -1,20 +1,22 @@
 import random
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InputMediaVideo
 from Lucyxbot import app
-from config import PIC, BOT_USERNAME
+from config import PIC, BOT_USERNAME, VIDEO, SUPPORT_GROUP, UPDATE_CHANNEL
+
 
 @app.on_message(filters.command("start"))
 async def start(_, msg):
     buttons = [[
             InlineKeyboardButton('🥀 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘs 🥀', url=f'https://t.me/{BOT_USERNAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🥀 Sᴜᴘᴘᴏʀᴛ 🥀', url='https://t.me/vkmovies02'),
-            InlineKeyboardButton('🥀 Uᴘᴅᴀᴛᴇs 🥀', url='https://t.me/vkmovies2')
+            InlineKeyboardButton('🥀 Sᴜᴘᴘᴏʀᴛ 🥀', url='https://t.me/{SUPPORT_GROUP}'),
+            InlineKeyboardButton('🥀 Uᴘᴅᴀᴛᴇs 🥀', url='https://t.me/{UPDATE_CHANNEL}')
     ],[
             InlineKeyboardButton('✨ Cᴏᴍᴍᴀɴᴅs ✨', callback_data="commands")
     ],[
-            InlineKeyboardButton('✨ Sᴏᴜʀᴄᴇ ✨', url=f'https://t.me/{BOT_USERNAME}?start=true')
+            InlineKeyboardButton('✨ Sᴏᴜʀᴄᴇ ✨', callback_data="source")
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await msg.reply_photo(
@@ -23,31 +25,58 @@ async def start(_, msg):
         reply_markup=reply_markup
     )
 
+@app.on_message(filters.command(["help"]) & filters.group)
+async def help_command(client, message):
+    start_button_link = f"https://t.me/{BOT_USERNAME}?start=your_start_parameter"
+    caption = "⟳ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ᴍʏ ʜᴇʟᴘ ᴍᴇɴᴜ ɪɴ ʏᴏᴜʀ ᴘᴍ "
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton("⟳ ʜᴇʟᴘ ⟳", url=start_button_link)],
+        ]
+    )
+
+    await message.reply_text(caption, reply_markup=keyboard)
 @app.on_callback_query()
-def callback_query_handler(client, query):
-    if query.data == 'commands':
+async def callback_query_handler(client, query):
+    if query.data == 'home':
+        ghelp_text1 = (
+             """𝙷𝙴𝚈 𝚃𝙷𝙴𝚁𝙴! 🥀 ,\n👋 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝒕𝒐 <a href='https://t.me/lucy666xbot'>➳❥ɪᴹ᭄𝑳𝒖𝒄𝒚 𝒙 𝒃𝒐𝒕 🫧</a>,\n\n<b>𝙸 𝚊𝚖 𝚑𝚎𝚛𝚎 𝚝𝚘 𝚊𝚜𝚜𝚒𝚜𝚝 𝚢𝚘𝚞 𝚠𝚒𝚝𝚑 𝚟𝚊𝚛𝚒𝚘𝚞𝚜 𝚝𝚊𝚜𝚔𝚜. 𝙵𝚎𝚎𝚕 𝚏𝚛𝚎𝚎 𝚝𝚘 𝚎𝚡𝚙𝚕𝚘𝚛𝚎 𝚝𝚑𝚎 𝚊𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚊𝚗𝚍 𝚏𝚎𝚊𝚝𝚞𝚛𝚎𝚜.</b>\n\n"""
+        )
+        buttons1 = [[
+            InlineKeyboardButton('🥀 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘs 🥀', url=f'https://t.me/{BOT_USERNAME}?startgroup=true')
+            ],[
+            InlineKeyboardButton('🥀 Sᴜᴘᴘᴏʀᴛ 🥀', url='https://t.me/vkmovies02'),
+            InlineKeyboardButton('🥀 Uᴘᴅᴀᴛᴇs 🥀', url='https://t.me/vkmovies2')
+    ],[
+            InlineKeyboardButton('✨ Cᴏᴍᴍᴀɴᴅs ✨', callback_data="commands")
+    ],[
+            InlineKeyboardButton('✨ Sᴏᴜʀᴄᴇ ✨', callback_data="source")
+    ]]
+        reply_markup1 = InlineKeyboardMarkup(buttons1)
+        await query.message.edit_text(ghelp_text1, reply_markup=reply_markup1)
+    elif query.data == 'commands':
         ghelp_text = (
              " <b>Pᴏᴡᴇʀs Oғ </b><a href='https://t.me/lucy666xbot'>➳❥ɪᴹ᭄𝑳𝒖𝒄𝒚 𝒙 𝒃𝒐𝒕 🫧</a> "
         )
-        
-        buttons = [
-            [
-                InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
-            ]
-        ]
+        buttons = [[
+            InlineKeyboardButton("⟲ ʙᴀᴄᴋ ⟳", callback_data="home"),
+            InlineKeyboardButton("⟳ ᴄʟᴏsᴇ ⟳", callback_data="close")
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-
-        
-        query.message.edit_text(ghelp_text, reply_markup=reply_markup)
-        
-
-# Define the callback query handler for button clicks
-@app.on_callback_query()
-def button_click(client, callback_query):
-    # Get the callback data to determine which button was clicked
-    callback_data = callback_query.data
-
-    # Handle close button
-    if callback_data == "close":
-        # Delete the message with the close button
-        client.delete_messages(chat_id=callback_query.msg.chat.id, message_ids=callback_query.msg.message_id)
+        await query.message.edit_text(ghelp_text, reply_markup=reply_markup)
+    elif query.data == "source":
+        await query.edit_message_media(
+        media=InputMediaVideo(VIDEO, has_spoiler=True),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="⟳ ʙᴀᴄᴋ ⟳", callback_data="close")]
+            ]
+        ),
+        )
+    elif query.data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
