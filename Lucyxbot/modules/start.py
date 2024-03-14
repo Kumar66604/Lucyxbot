6909279faa21d4260a6101316cb8eb22fcf88a03
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-14 17:28:06 +0530

    Update start.py

--- Lucyxbot/modules/start.py
+++ Lucyxbot/modules/start.py
@@ -14,7 +14,7 @@ async def start(_, msg):
     ],[
             InlineKeyboardButton('✨ Cᴏᴍᴍᴀɴᴅs ✨', callback_data="commands")
     ],[
-            InlineKeyboardButton('✨ Sᴏᴜʀᴄᴇ ✨', url=f'https://t.me/{BOT_USERNAME}?start=true')
+            InlineKeyboardButton('✨ Sᴏᴜʀᴄᴇ ✨', callback_data="about")
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await msg.reply_photo(
@@ -38,26 +38,17 @@ def callback_query_handler(client, query):
 
 
 @app.on_callback_query()
-def callback_query_handler(client, query):
-    if query.data == 'home_':
-        ghelp_text = (
-             """𝙷𝙴𝚈 𝚃𝙷𝙴𝚁𝙴! 🥀 {},\n👋 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝒕𝒐 <a href='https://t.me/lucy666xbot'>➳❥ɪᴹ᭄𝑳𝒖𝒄𝒚 𝒙 𝒃𝒐𝒕 🫧</a>,\n\n<b>𝙸 𝚊𝚖 𝚑𝚎𝚛𝚎 𝚝𝚘 𝚊𝚜𝚜𝚒𝚜𝚝 𝚢𝚘𝚞 𝚠𝚒𝚝𝚑 𝚟𝚊𝚛𝚒𝚘𝚞𝚜 𝚝𝚊𝚜𝚔𝚜. 𝙵𝚎𝚎𝚕 𝚏𝚛𝚎𝚎 𝚝𝚘 𝚎𝚡𝚙𝚕𝚘𝚛𝚎 𝚝𝚑𝚎 𝚊𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚊𝚗𝚍 𝚏𝚎𝚊𝚝𝚞𝚛𝚎𝚜.</b>\n\n"""
-        )
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
         buttons = [[
-            InlineKeyboardButton('🥀 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘs 🥀', url=f'https://t.me/{BOT_USERNAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🥀 Sᴜᴘᴘᴏʀᴛ 🥀', url='https://t.me/vkmovies02'),
-            InlineKeyboardButton('🥀 Uᴘᴅᴀᴛᴇs 🥀', url='https://t.me/vkmovies2')
-    ],[
-            InlineKeyboardButton('✨ Cᴏᴍᴍᴀɴᴅs ✨', callback_data="commands")
-    ],[
-            InlineKeyboardButton('✨ Sᴏᴜʀᴄᴇ ✨', url=f'https://t.me/{BOT_USERNAME}?start=true')
-    ]]
+            InlineKeyboardButton("🔒 Close", callback_data = "close")
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        query.message.edit_text(ghelp_text.format(msg.from_user.mention), reply_markup=reply_markup)
-    
-# Additional callback for closing the message
-@app.on_callback_query(filters.regex("^close"))
-async def close_callback(_, query):
-    chat_id = query.message.chat.id
-    await query.message.delete()
+        await query.message.edit_text(ghelp_text, reply_markup=reply_markup)
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
