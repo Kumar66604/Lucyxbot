a9290f360c1c85a1aadae7dec76a0c1ffcbcc1c8
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-16 15:41:33 +0530

    Update and rename chatgpt.py to ai.py

--- Lucyxbot/modules/ai.py
+++ Lucyxbot/modules/ai.py
@@ -38,3 +38,27 @@ async def chat_gpt(bot, message):
                 await message.reply_text("Error accessing the response.")
     except Exception as e:
         await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+
+@app.on_message(filters.command(["assis"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def chat(app :app, message):
+    
+    try:
+        start_time = time.time()
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "**ʜᴇʟʟᴏ sɪʀ**\n**ᴇxᴀᴍᴘʟᴇ:-**`.assis How to set girlfriend ?`")
+        else:
+            a = message.text.split(' ', 1)[1]
+            MODEL = "gpt-3.5-turbo"
+            resp = openai.ChatCompletion.create(model=MODEL,messages=[{"role": "user", "content": a}],
+    temperature=0.2)
+            x=resp['choices'][0]["message"]["content"]
+            text = x    
+            tts = gTTS(text, lang='hi')
+            tts.save('output.mp3')
+            await app.send_voice(chat_id=message.chat.id, voice='output.mp3')
+            os.remove('output.mp3')            
+            
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ") 
