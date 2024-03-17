1d68d4494069459e96ad5ad679be4ee7d240040c
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-17 21:07:04 +0530

    Update ai.py

--- Lucyxbot/modules/ai.py
+++ Lucyxbot/modules/ai.py
@@ -9,7 +9,7 @@ from pyrogram import filters
 
 API_URL = "https://sugoi-api.vercel.app/search"
 
-@app.on_message(filters.command(["ucy","chatgpt","ai","ask","gpt","solve"],  prefixes=["L","+", ".", "/", "-", "", "$","#","&"]))
+@app.on_message(filters.command(["ucy","chatgpt"],  prefixes=["L","+", ".", "/", "-", "", "$","#","&"]))
 async def chat_gpt(bot, message):
     try:
         start_time = time.time()
@@ -41,31 +41,7 @@ async def chat_gpt(bot, message):
     except Exception as e:
         await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
 
-@app.on_message(filters.command(["assis"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
-async def chat(app :app, message):
-    
-    try:
-        start_time = time.time()
-        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
-        if len(message.command) < 2:
-            await message.reply_text(
-            "**ʜᴇʟʟᴏ sɪʀ**\n**ᴇxᴀᴍᴘʟᴇ:-**`.assis How to set girlfriend ?`")
-        else:
-            a = message.text.split(' ', 1)[1]
-            MODEL = "gpt-3.5-turbo"
-            resp = openai.ChatCompletion.create(model=MODEL,messages=[{"role": "user", "content": a}],
-    temperature=0.2)
-            x=resp['choices'][0]["message"]["content"]
-            text = x    
-            tts = gTTS(text, lang='hi')
-            tts.save('output.mp3')
-            await app.send_voice(chat_id=message.chat.id, voice='output.mp3')
-            os.remove('output.mp3')            
-            
-    except Exception as e:
-        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ") 
-
-@app.on_message(filters.command("bingsearch"))
+@app.on_message(filters.command("bing"))
 async def bing_search(michiko, message):
     try:
         if len(message.command) == 1:
