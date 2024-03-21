e076d02e079c61575ea26afa146613de656db5ac
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-03-21 23:33:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,3 +12,18 @@ VIDEO = getenv("VIDEO","https://graph.org/file/8926caeb4948c47b12080.mp4")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP","vkmovies02")
 UPDATE_CHANNEL = getenv("UPDATE_CHANNEL","vkmovies2")
 RAPID_APIKEY = getenv("RAPID_APIKEY", "")
+
+# Admins, Channels & Users
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_channel = environ.get('AUTH_CHANNEL')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+
+# MongoDB information
+DATABASE_URI = environ['DATABASE_URI']
+DATABASE_NAME = environ['DATABASE_NAME']
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
